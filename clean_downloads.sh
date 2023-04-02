ef15dcec5a1f768f103d4c78c61c464780869b3e
Thomas Cope <13124675+copethomas@users.noreply.github.com>
2023-04-02 10:58:00 +0100

    Update clean_downloads.sh

--- clean_downloads.sh
+++ clean_downloads.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #Cleans downloads older then one week
 #-Thomas Cope
+#
+# Crontab line:
+# 0 21 * * * /home/tom/bin/clean_downloads.sh
+#
 find /home/tom/Downloads/ -type f -mtime +7 -delete
